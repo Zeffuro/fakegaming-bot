9b498c0d5748009826fdec46e868afd376fed262
Zeffuro <Jeffroiscool@gmail.com>
2025-09-22 23:37:28 +0200

    Adjust script

--- scripts/generate-command-table.js
+++ scripts/generate-command-table.js
@@ -1,9 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath, pathToFileURL} from 'url';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const modulesPath = path.join(__dirname, '../modules');
 
-function findCommandDirs(modulesPath) {
+async function findCommandDirs(modulesPath) {
     const moduleFolders = fs.readdirSync(modulesPath);
     const commandDirs = [];
     for (const folder of moduleFolders) {
@@ -15,14 +17,16 @@ function findCommandDirs(modulesPath) {
     return commandDirs;
 }
 
-function loadCommands(commandDirs) {
+async function loadCommands(commandDirs) {
     const commands = [];
     for (const dir of commandDirs) {
         const files = fs.readdirSync(dir).filter(f => f.endsWith('.js') || f.endsWith('.ts'));
         for (const file of files) {
             const cmdPath = path.join(dir, file);
             try {
-                const cmd = require(cmdPath).default || require(cmdPath);
+                // Use dynamic import for ESM
+                const commandModule = await import(pathToFileURL(cmdPath).href);
+                const cmd = commandModule.default || commandModule;
                 if (cmd && cmd.data && cmd.data.name && cmd.data.description) {
                     commands.push({
                         name: cmd.data.name,
@@ -30,8 +34,8 @@ function loadCommands(commandDirs) {
                         permissions: cmd.permissions || 'All users'
                     });
                 }
-            } catch {
-                // Ignore files that can't be required (e.g. not a command)
+            } catch (e) {
+                // Ignore files that can't be imported
             }
         }
     }
@@ -46,22 +50,26 @@ function generateTable(commands) {
     return table;
 }
 
-const commandDirs = findCommandDirs(modulesPath);
-const commands = loadCommands(commandDirs);
-const table = generateTable(commands);
+async function main() {
+    const commandDirs = await findCommandDirs(modulesPath);
+    const commands = await loadCommands(commandDirs);
+    const table = generateTable(commands);
 
-const readmePath = path.join(__dirname, '../README.md');
-let readme = fs.readFileSync(readmePath, 'utf8');
-const start = '<!-- COMMAND_TABLE_START -->';
-const end = '<!-- COMMAND_TABLE_END -->';
-const regex = new RegExp(`${start}[\\s\\S]*?${end}`, 'm');
+    const readmePath = path.join(__dirname, '../README.md');
+    let readme = fs.readFileSync(readmePath, 'utf8');
+    const start = '<!-- COMMAND_TABLE_START -->';
+    const end = '<!-- COMMAND_TABLE_END -->';
+    const regex = new RegExp(`${start}[\\s\\S]*?${end}`, 'm');
 
-const newSection = `${start}\n${table}${end}`;
-if (regex.test(readme)) {
-    readme = readme.replace(regex, newSection);
-} else {
-    readme += `\n\n${newSection}\n`;
+    const newSection = `${start}\n${table}${end}`;
+    if (regex.test(readme)) {
+        readme = readme.replace(regex, newSection);
+    } else {
+        readme += `\n\n${newSection}\n`;
+    }
+
+    fs.writeFileSync(readmePath, readme);
+    console.log('README.md command table updated!');
 }
 
-fs.writeFileSync(readmePath, readme);
-console.log('README.md command table updated!');
+main();
