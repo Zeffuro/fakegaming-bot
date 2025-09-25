30da308336a3a1d2639d0bd3fc369e06ad76da46
Zeffuro <Jeffroiscool@gmail.com>
2025-09-25 16:48:09 +0200

    Whoops

--- packages/common/src/models/patch-subscription-config.ts
+++ packages/common/src/models/patch-subscription-config.ts
@@ -1,7 +1,12 @@
 import {Table, Column, Model, DataType, Unique} from 'sequelize-typescript';
 
-@Unique('unique_game_channel_patch_subscription')
-@Table
+@Table({
+    uniqueKeys: {
+        unique_game_channel_patch_subscription: {
+            fields: ['game', 'channelId']
+        }
+    }
+})
 export class PatchSubscriptionConfig extends Model {
     @Column(DataType.STRING)
     declare game: string;
