065a3eedd0ab49f4fd1150ca7609e73c41e76a9a
Zeffuro <Jeffroiscool@gmail.com>
2025-10-04 13:31:50 +0200

    Fix logout

--- packages/dashboard/app/api/auth/logout/route.ts
+++ packages/dashboard/app/api/auth/logout/route.ts
@@ -1,17 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getBaseUrl } from "@/lib/util/getBaseUrl";
 
 export async function GET(req: NextRequest) {
-  // Create response that redirects to home
-  const response = NextResponse.redirect(new URL("/", req.url));
+    // Create response that redirects to home
+    const response = NextResponse.redirect(`${getBaseUrl(req)}/`);
 
-  // Clear JWT cookie
-  response.cookies.set({
-    name: "jwt",
-    value: "",
-    path: "/",
-    expires: new Date(0),
-    sameSite: "lax",
-  });
+    // Clear JWT cookie
+    response.cookies.set({
+        name: "jwt",
+        value: "",
+        path: "/",
+        expires: new Date(0),
+        sameSite: "lax",
+    });
 
-  return response;
+    return response;
 }
